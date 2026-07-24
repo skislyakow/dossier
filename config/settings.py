b1b0be3830b9b895884097d1a1ef24119bd6a6b7
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-07-24 20:04:47 +0300

    add kislyakov.pro domain to ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['132.243.121.192', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['132.243.121.192', 'kislyakov.pro', 'www.kislyakov.pro', 'localhost', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['http://132.243.121.192']
+CSRF_TRUSTED_ORIGINS = ['http://132.243.121.192', 'https://kislyakov.pro', 'https://www.kislyakov.pro']
 
 
 # Application definition
